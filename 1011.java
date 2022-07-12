a98dc0747478eb57ecb964ae9b6eaf2c49829bc0
Elfina <94973185+Minjiaaa@users.noreply.github.com>
2022-07-12 23:55:12 +0800

    Update 1011.java

--- 1011.java
+++ 1011.java
@@ -1,23 +1,57 @@
 class Solution{
-    public int shipWithinDays(int[] weights, int d){
-        int max = 0, sum = 0;
-        for (int weight : weights){
-            max = Math.max(max, w);//求最大值的方法
-            sum += w;
-        }
-        int lo = max, hi = sum;
-        while (lo < hi){ //什么时候加等于，什么时候不加等于呢？
-            int mid = lo + (hi - lo) / 2;
-            if (withInDays(weight, mid, d)){
-                r = mid;
+    public int shipWithinDays(int[] weights, int d){ //从数组的最大元素开始遍历判断值i是否满足verification
+        // 二分查找 r =数组的总和， l=数组的最大值
+        int r = Arrays.stream(weights).sum();
+        int l = Arrays.stream(weights).max().getAsInt();
+        // l < r
+        while(l < r) {
+            int mid = (l+r)/2; //取中间值
+            if(verification(weights, d, mid)){ //包括mid
+                r = mid; //
             } else {
-                l = mid + 1;
+                l = mid + 1；//逼近左指针
             }
         }
-        return r;
-     }       
-     public boolean withInDays(int[] weights, int d,int ship){
-        int day = d, remain = ship;
+        return l; //返回当前l就是最小的满足条件的值，即最低运载能力
+    }
+    //判断最低运载能力为H的时候能否在d天内送达
+     public boolean verification(int[] weights, int d,int H){
+        int count = 1; //天数计数，天数初始化为1
+        int singleWeight = 0； //每天的包裹总量
+        for (int i = 0; i < weights.length; ++i){
+            singleWeights += weights[i];//累计包裹总量
+            if (singleWeights > H) { //如果累计包裹总量singleWeight>H，天数+1
+                ++count;
+                singleWeight = weights[i]； 
+            }
+            //如果当前累计的天数count>d, 说明当前H不满足条件，返回false
+            if (count > d){
+                return false;
+            }
+        }
+        return true; //说明当前H满足条件，返回true
+     }
+ }
+
+class Solution{
+    public int shipWithinDays(int[] weights, int d){
+        int lo = 0, hi = 0;
+        for (int weight : weights){
+            hi += weight;
+        }
+        while (hi-lo > 1){
+            int mid = (hi + lo) / 2;
+            if (withInDays(weights, d, mid) == false){
+                lo = mid;
+            } else {
+                hi = mid;
+            }
+        }
+        return hi;
+    }
+    
+    public boolean withInDays(int[] weights, int d, int ship){
+        int day = d, remain = ship;// 剩余天数,承载量
         for (int w : weights){
             if (w > ship)
                 return false;
@@ -31,6 +65,6 @@ class Solution{
         }
         return true;
      }
- }
+}
        
             
